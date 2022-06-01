203cc4297850ac355457c118a3a9822c53f07466
David Long <74575072+DLong86@users.noreply.github.com>
2022-06-01 16:49:46 +0900

    Update app.js

--- Project #2/app.js
+++ Project #2/app.js
@@ -2,7 +2,7 @@ const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
 const search = document.querySelector('.search input');
 
-// Function we want to use for when a user passes in a new todo
+
 const generateTemplate = todo => {
 
     const html = `
@@ -12,17 +12,17 @@ const generateTemplate = todo => {
     </li>
     `;
 
-    list.innerHTML += html; // this will add the new todo to the html document the += will add not override
+    list.innerHTML += html; 
 
 }
 
-addForm.addEventListener('submit', e => { // listening for a submit event
+addForm.addEventListener('submit', e => { 
     e.preventDefault();
-    const todo = addForm.add.value.trim(); // trim will clear any spaces before or after text
+    const todo = addForm.add.value.trim(); 
 
-    if(todo.length){ // todo.length means that there needs to be text, no text will not submit the new todo!
-        generateTemplate(todo); // calling the function
-        addForm.reset(); // Resets all text inside the todo submit
+    if(todo.length){ 
+        generateTemplate(todo); 
+        addForm.reset(); 
     }
 });
 
